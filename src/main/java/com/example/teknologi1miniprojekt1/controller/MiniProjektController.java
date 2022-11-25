1532946d82437e52e48289dd0d0b40c5c385f607
TobiasSolv <tobiasurban@hotmail.com>
2022-11-25 15:46:35 +0100

    made webservice.html

--- src/main/java/com/example/teknologi1miniprojekt1/controller/MiniProjektController.java
+++ src/main/java/com/example/teknologi1miniprojekt1/controller/MiniProjektController.java
@@ -21,14 +21,44 @@ public class MiniProjektController {
         return "area 2";
     }
 
-    @GetMapping("/area 2")
+    @GetMapping("/area 3")
     public String area3(){
-        return "area 2";
+        return "area 3";
     }
 
-    @GetMapping("/area 2")
+    @GetMapping("/area 4")
     public String area4(){
-        return "area 2";
+        return "area 4";
+    }
+
+    @GetMapping("/area 5")
+    public String area5(){
+        return "area 5";
+    }
+
+    @GetMapping("/area 6")
+    public String area6(){
+        return "area 6";
+    }
+
+    @GetMapping("/area 7")
+    public String area7(){
+        return "area 7";
+    }
+
+    @GetMapping("/area 8")
+    public String area8(){
+        return "area 8";
+    }
+
+    @GetMapping("/area 9")
+    public String area9(){
+        return "area 9";
+    }
+
+    @GetMapping("/area 10")
+    public String area10(){
+        return "area 10";
     }
 
     @GetMapping("/webservice")
